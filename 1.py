a91b202f0152f993687dd34deb913905df64e837
Alex-Shubin <alex.cuo@mail.ru>
2025-01-22 21:51:07 +0300

    commit 1.py

---
+++ 1.py
@@ -0,0 +1,7 @@
+user_value1 = input("Введите первое значение: ")
+user_value2 = input("Введите второе значение: ")
+user_value3 = input("Введите третье значение: ")
+
+print(user_value1, user_value2, user_value3)
+print(user_value1, user_value2, user_value3, sep=" - ")
+print("- " + user_value1, user_value2, user_value3, sep="\n- ")
